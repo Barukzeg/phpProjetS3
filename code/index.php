f53486fecb9403e9cbd66dcee8e2b6704a0065c5
Titouan Herbaut <titouan.herbaut@etu.iut-tlse3.fr>
2023-12-01 10:23:13 +0100

    delete affichage.php

--- code/index.php
+++ code/index.php
@@ -1,3 +1,16 @@
 <?php
-    echo "Hello World !"
+
+     ///Connexion au serveur MySQL
+     $server = "localhost";
+     $db="cabinet_medical";
+     $login="carnetadresse";
+     $mdp="password";
+     try {
+         $mysqlClient = new PDO("mysql:host=$server;dbname=$db", $login, $mdp);
+     }
+     catch (Exception $e) {
+         die('Erreur : ' . $e->getMessage());
+     };
+
+
 ?>
