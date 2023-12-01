b8d98a32ec1889a70305872c374a16b9fbe93607
Nail Lamarti <nail.lamarti@etu.iut-tlse3.fr>
2023-12-01 10:23:01 +0100

    class Personne
    start

--- code/pages/personne.php
+++ code/pages/personne.php
@@ -1,3 +1,47 @@
 <<?php
-    echo "Hello World";
+    class Personne {
+        private $idPersonne;
+        private $nom;
+        private $prenom;
+        private $civilite;
+
+        public function __construct($idPersonne, $nom, $prenom, $civilite) {
+            $this->idPersonne = $idPersonne;
+            $this->nom = $nom;
+            $this->prenom = $prenom;
+            $this->civilite = $civilite;
+        }
+
+        public function getIdPersonne() {
+            return $this->idPersonne;
+        }
+
+        public function getNom() {
+            return $this->nom;
+        }
+
+        public function getPrenom() {
+            return $this->prenom;
+        }
+
+        public function getCivilite() {
+            return $this->civilite;
+        }
+
+        public function setIdPersonne($id) {
+            $this->idPersonne = $idPersonne;
+        }
+
+        public function setNom($nom) {
+            $this->nom = $nom;
+        }
+
+        public function setPrenom($prenom) {
+            $this->prenom = $prenom;
+        }
+
+        public function setCivilite($civilite) {
+            $this->civilite = $civilite;
+        }
+    }
 ?>
