daab86d97a4db1848c46a4161a873f5354a85920
Nail Lamarti <nail.lamarti@etu.iut-tlse3.fr>
2023-12-02 19:03:48 +0100

    update Personne
    update of getById

--- code/modele/personne.php
+++ code/modele/personne.php
@@ -43,21 +43,26 @@
         public function setCivilite($civilite) {
             $this->civilite = $civilite;
         }
-    }
 
-    public static function getById($id) {
-        $bd = BDD::getBDD() -> getConnection(); // connexion
-        $query = $db->prepare("SELECT * FROM personnes WHERE idPersonne = :id");
-        $query->bindParam(':id', $id);
-        $query->execute();
+        public static function getById($id) {
 
-        $result = $query->fetch(PDO::FETCH_ASSOC);
-
-        if ($result) {
-            return new Personne($result['idPersonne'], $result['nom'], $result['prenom'], $result['civilite']);
-        } else {
-            return null;
+            // connexion
+            $bd = BDD::getBDD() -> getConnection();
+    
+            // requete
+            $query = $db->prepare("SELECT * FROM personnes WHERE idPersonne = :id");
+            $query->bindParam(':id', $id);
+    
+            // execution
+            $query->execute();
+            $result = $query->fetch(PDO::FETCH_ASSOC);
+    
+            // retour d'une instance de Personne
+            if ($result) {
+                return new Personne($result['idPersonne'], $result['nom'], $result['prenom'], $result['civilite']);
+            } else {
+                return null;
+            }
         }
     }
-
 ?>
