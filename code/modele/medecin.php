2e9bb96223947a0440b2243db8a22a24080517ae
Nail Lamarti <nail.lamarti@etu.iut-tlse3.fr>
2023-12-02 19:04:11 +0100

    update Medecin
    added geById

--- code/modele/medecin.php
+++ code/modele/medecin.php
@@ -14,5 +14,26 @@
         public function setIdMedecin($idMedecin) {
             $this->idMedecin = $idMedecin;
         }
+
+        public static function getById($id) {
+
+            // connexion
+            $db = BDD::getBDD()->getConnection();
+
+            // requete
+            $query = $db->prepare("SELECT * FROM Personne p INNER JOIN Medecin m ON p.idPersonne = m.idMedecin WHERE m.idMedecin = :id");
+            $query->bindParam(':id', $id);
+
+            // execution
+            $query->execute();
+            $result = $query->fetch(PDO::FETCH_ASSOC);
+
+            // retour d'une instance de Medecin
+            if ($result) {
+                return new Medecin($result['idMedecin'], $result['nom'], $result['prenom'], $result['civilite']);
+            } else {
+                return null;
+            }
+        }
     }
 ?>
