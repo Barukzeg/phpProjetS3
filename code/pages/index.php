78e7a3afbd45c4aab77813474e29061162f07f48
Titouan Herbaut <titouan.herbaut@etu.iut-tlse3.fr>
2023-12-13 18:25:34 +0100

    update index and add images

--- code/pages/index.php
+++ code/pages/index.php
@@ -1,22 +1,23 @@
 <html>
-    <head>
+    <header>
         <meta charset="UTF-8">
         <title>Page d'accueil</title>
+        <link href="../style/style.css" rel="stylesheet">
         <div>
             <h1>Bienvenue sur votre outil de gestion de votre Cabinet Médical</h1>
         </div>
-    </head>
+    </header>
     <body>
         <div class="boite-boutons">
-            <div class="btn-1" href="/Usager/listeUsager.php">
-                <img src="" alt="">
+            <div class="btn" href="/Usager/listeUsager.php">
+                <img src="../image/iconUsager.png" alt="icon User">
                 <h3>Accès à la liste des usagers</h3>
             </div>
-            <div class="btn-2">
-                <img src="" alt="">
+            <div class="btn">
+                <img src="../image/iconMedecin.png" alt="">
                 <h3>Accès à la liste des médecins</h3>
             </div>
-            <div class="btn-3">
+            <div class="btn">
                 <img src="" alt="">
                 <h3>Accès à la liste des rendez-vous</h3>
             </div>
