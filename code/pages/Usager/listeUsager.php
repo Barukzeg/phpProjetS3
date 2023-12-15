724215fdc47e6eda032b64d95cc9ba11046bf788
Titouan Herbaut <titouan.herbaut@etu.iut-tlse3.fr>
2023-12-15 09:50:32 +0100

    update listeUsager

--- code/pages/Usager/listeUsager.php
+++ code/pages/Usager/listeUsager.php
@@ -5,18 +5,6 @@
             include "../../modele/bdd.php";
             include "../../modele/usager.php";
 
-            ///Connexion au serveur MySQL
-            $server = "localhost";
-            $db="cabinet_medical";
-            $login="root";
-            $mdp="";
-            try {
-                $mysqlClient = new PDO("mysql:host=$server;dbname=$db", $login, $mdp);
-            }
-            catch (Exception $e) {
-                die('Erreur : ' . $e->getMessage());
-            };
-
             $getPersonne = "Select * FROM personne where IdPersonne in (SELECT IdUsager FROM usager)";
             $resultat = $mysqlClient->query($getPersonne);
             //$resultat = Usager::getAll();
