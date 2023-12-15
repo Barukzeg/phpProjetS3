068298b9a57796f1b0cd7d70c0f8778717f6962b
Titouan Herbaut <titouan.herbaut@etu.iut-tlse3.fr>
2023-12-15 10:15:32 +0100

    update everything

---
+++ code/pages/header.php
@@ -0,0 +1,23 @@
+<html>
+    <header>
+        <meta charset="UTF-8">
+        <title>Page d'accueil</title>
+        <link href="../style/style.css" rel="stylesheet">
+        <div class="boite-boutons">
+            <div class="btn">
+                <a href="./Usager/listeUsager.php">
+                    <img src="../image/iconUsager.png" alt="icon User">
+                    <h3>Accès à la liste des usagers</h3>
+                </a>
+            </div>
+            <div class="btn">
+                <img src="../image/iconMedecin.png" alt="icon Medecin">
+                <h3>Accès à la liste des médecins</h3>
+            </div>
+            <div class="btn">
+                <img src="../image/iconRDV.png" alt="icon RDV">
+                <h3>Accès à la liste des rendez-vous</h3>
+            </div>
+        </div>
+    </header>
+</html>
--- code/pages/index.php
+++ code/pages/index.php
@@ -1,28 +1,10 @@
 <html>
     <header>
-        <meta charset="UTF-8">
-        <title>Page d'accueil</title>
-        <link href="../style/style.css" rel="stylesheet">
-        <div>
-            <h1>Bienvenue sur votre outil de gestion de votre Cabinet Médical</h1>
-        </div>
+        <?php include "header.php"; ?>
     </header>
     <body>
-        <div class="boite-boutons">
-            <div class="btn">
-                <a href="./Usager/listeUsager.php">
-                    <img src="../image/iconUsager.png" alt="icon User">
-                    <h3>Accès à la liste des usagers</h3>
-                </a>
-            </div>
-            <div class="btn">
-                <img src="../image/iconMedecin.png" alt="icon Medecin">
-                <h3>Accès à la liste des médecins</h3>
-            </div>
-            <div class="btn">
-                <img src="" alt="">
-                <h3>Accès à la liste des rendez-vous</h3>
-            </div>
+        <div>
+            <h1>Bienvenue sur votre outil de gestion de votre Cabinet Médical</h1>
         </div>
     </body>
 </html>
