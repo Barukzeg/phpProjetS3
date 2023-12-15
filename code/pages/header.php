7ec67ef35734e8cae8356e06044564b2e07d3575
Titouan Herbaut <titouan.herbaut@etu.iut-tlse3.fr>
2023-12-15 11:08:13 +0100

    update header

--- code/pages/header.php
+++ code/pages/header.php
@@ -3,20 +3,33 @@
         <meta charset="UTF-8">
         <title>Page d'accueil</title>
         <link href="../style/style.css" rel="stylesheet">
+        <div class="boite-logo">
+            <img src="../image/placeholder.webp" alt="logo cabinet medical">
+        </div>
         <div class="boite-boutons">
             <div class="btn">
                 <a href="./Usager/listeUsager.php">
                     <img src="../image/iconUsager.png" alt="icon User">
-                    <h3>Accès à la liste des usagers</h3>
+                    <p>Accès à la liste des usagers</p>
                 </a>
             </div>
             <div class="btn">
-                <img src="../image/iconMedecin.png" alt="icon Medecin">
-                <h3>Accès à la liste des médecins</h3>
+                <a href="">
+                    <img src="../image/iconMedecin.png" alt="icon Medecin">
+                    <p>Accès à la liste des médecins</p>
+                </a>
             </div>
             <div class="btn">
-                <img src="../image/iconRDV.png" alt="icon RDV">
-                <h3>Accès à la liste des rendez-vous</h3>
+                <a href="">
+                    <img src="../image/iconRDV.png" alt="icon RDV">
+                    <p>Accès à la liste des rendez-vous</p>
+                </a>
+            </div>
+            <div class="btn">
+                <a href="">
+                    <img src="../image/iconStat.png" alt="icon Statistique">
+                    <p>Accès aux statistiques</p>
+                </a>
             </div>
         </div>
     </header>
