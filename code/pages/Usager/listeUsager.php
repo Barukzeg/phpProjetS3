a9f28d0928a02d751697cdd3df1a70e24ee47f8c
Titouan <titouan.herbaut@etu.iut-tlse3.fr>
2023-12-27 22:36:17 +0100

    update page listeUsager and add css for it

--- code/pages/Usager/listeUsager.php
+++ code/pages/Usager/listeUsager.php
@@ -1,52 +1,58 @@
 <html>
     <head>
         <title>Liste des usagers</title>
-        <link rel="stylesheet" href="../../style/style.css">
+        <link rel="stylesheet" href="/phpProjetS3/code/style/style.css">
+        <link rel="stylesheet" href="/phpProjetS3/code/style/listeUsager.css">
     </head>
+    <!-- TODO réparer le header
+    parce que la ca marche mais c'est chemin absolu
+    dcp c'est pas bien -->
     <?php include "../header.php"; ?>
     <body>
-        <h1>Usagers :</h1>
-        <?php
-            include "../../services/serviceUsager.php";
-            include_once "../../modele/Usager.php";
+        <div class="content">
+            <h1>Liste des usagers :</h1>
+            <div class="bouton-add">
+                <form action="addUsager.php">
+                    <button>Ajouter un usager</button>
+                </form>
+            </div>
+            <?php
+                include "../../services/serviceUsager.php";
+                include_once "../../modele/Usager.php";
 
-            try{
-                $resultat = serviceUsager::getService()->getUsagerAlpha();
-                echo '<div class="affichageResult">';
-                foreach ($resultat as $row) {
-                    switch($row->getCivilite()){
-                        case 'M':
-                            $sexe = 'Homme';
-                            break;
-                        case 'F':
-                            $sexe = 'Femme';
-                            break;
-                        case 'A':
-                            $sexe = 'Autre';
-                            break;
-                        default:
-                            $sexe = 'Non renseigné';
-                            break;
+                try{
+                    $resultat = serviceUsager::getService()->getUsagerAlpha();
+                    echo '<div class="affichageResult">';
+                    foreach ($resultat as $row) {
+                        switch($row->getCivilite()){
+                            case 'M':
+                                $sexe = 'Homme';
+                                break;
+                            case 'F':
+                                $sexe = 'Femme';
+                                break;
+                            case 'A':
+                                $sexe = 'Autre';
+                                break;
+                            default:
+                                $sexe = 'Non renseigné';
+                                break;
+                        }
+                        $ville = ucfirst($row->getLieuNaissance());
+                        echo '<div class="result">'
+                        .$row->getNom().' '
+                        .$row->getPrenom().'<br>'
+                        .'Sexe : '.$sexe.'<br>'
+                        .'Adresse : '.$row->getAdresseComplete().' '.$row->getCodePostal().'<br>'
+                        .'Date de naissance : '.$row->getDateNaissance()->format('d/m/Y').'<br>'
+                        .'Ville de naissance : '.$ville.'<br>'
+                        .'</div>';
                     }
-                    $ville = ucfirst($row->getLieuNaissance());
-                    echo '<div class="result">'
-                    .$row->getNom().' '
-                    .$row->getPrenom().'<br>'
-                    .'Sexe : '.$sexe.'<br>'
-                    .'Adresse : '.$row->getAdresseComplete().' '.$row->getCodePostal().'<br>'
-                    .'Date de naissance : '.$row->getDateNaissance()->format('d/m/Y').'<br>'
-                    .'Ville de naissance : '.$ville.'<br>'
-                    .'</div>';
+                    echo "</div>";
+                }catch (Exception $e){
+                    echo $e->getMessage();
                 }
-                echo "</div>";
-            }catch (Exception $e){
-                echo $e->getMessage();
-            }
-        ?>
-        <div class="bouton-add">
-            <form action="addUsager.php">
-                <button>Ajouter un usager</button>
-            </form>
+            ?>
         </div>
     </body>
 </html>
