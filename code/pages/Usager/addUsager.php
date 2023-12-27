1bab071bf266b52fcbd7dfd83b2ffcd8ee5deda0
Titouan <titouan.herbaut@etu.iut-tlse3.fr>
2023-12-27 22:38:17 +0100

    update addUsager / add css and traitement.php
    traitement for add in db

--- code/pages/Usager/addUsager.php
+++ code/pages/Usager/addUsager.php
@@ -1,72 +1,41 @@
 <!DOCTYPE HTML>
 <html>
-    <link rel="stylesheet" type="text/css" href="style.css"/>
+    <link rel="stylesheet" type="text/css" href="/phpProjetS3/code/style/style.css"/>
+    <link rel="stylesheet" type="text/css" href="/phpProjetS3/code/style/addUsager.css"/>
     <body>
         <div>
-            <h1>Saisie :</h1>
-            <?php
-                include "../../services/serviceUsager.php";
-                $url = "/listeUsager.php";
-                if (isset($_POST['envoie'])) {
-                    $nom = $_POST['nom'];
-                    $prenom = $_POST['prenom'];
-                    $civilite = $_POST['civilite'];
-                    $adresse = $_POST['adresse'];
-                    $codepostal = $_POST['codepostal'];
-                    $date = $_POST['dateNaissance'];
-                    $dateNaissance = new DateTime($date);
-                    $villeNaissance = $_POST['villeNaissance'];
-                    $numSecu = $_POST['numSecu'];
-                    $usager = new Usager(1,$nom, $prenom, $civilite, 1,$adresse, $codepostal, $dateNaissance, $villeNaissance, $numSecu);
-                    $repoUsager = new RepoUsager();
-                    $resultat = $repoUsager->addUsager($usager);
-                    if ($resultat) {
-                        echo "Usager ajouté";
-                    } else {
-                        echo "Erreur dans la requête : " . $mysqlClient->error;
-                    }
-                }else if(isset($_POST['reset'])){
-                    $url = "addUsager.php";
-                }
-                header("Location".$url)
-            ?>
-            <form action="addUsager.php" method="post">
-            <!-- TODO voir pour les trucs apres la
-                - int $idUsager
-                - int $idReferant 
-            -->
-                <div class="title-input">
-                    Nom : <input type="text" name="nom"><br>
-                </div>
-                <div>
-                    Prenom :<input type="text" name="prenom"><br>
-                </div>
-                <div>
-                    Civilité :<input type="text" name="civilite"><br>
-                </div>
-                <div>
-                    Adresse :<input type="text" name="adresse"><br>
-                </div>
-                <div>
-                    Code postal :<input type="text" name="codepostal"><br>
-                </div>
-                <div>
-                    Date de naissance :<input type="date" name="dateNaissance"><br>
-                </div>
-                <div>
-                    Ville de naissance :<input type="text" name="villeNaissance"><br>
-                </div>
-                <div>
-                    Numero de sécurité sociale :<input type="text" name="numSecu"><br>
-                </div>
+            <form action="traitement.php" method="post">
+                <label for="nom">Nom :</label>
+                <input type="text" id="nom" name="nom" required>
 
-                <div class="bouton-out">
-                    <form action="">
-                        <input type="reset" name="reset" value="Reset">
-                    </form>
-                    <form action="listeUsager.php">
-                        <input type="submit" name="envoie" value="Envoyer">
-                    </form>
+                <label for="prenom">Prénom :</label>
+                <input type="text" id="prenom" name="prenom" required>
+
+                <label for="civilite">Civilité :</label>
+                <select id="civilite" name="civilite" required>
+                    <option value="M">M</option>
+                    <option value="F">Mme</option>
+                    <option value="Autre">Autre/Ne se prononce pas</option>
+                </select>
+
+                <label for="adresse">Adresse :</label>
+                <input type="text" id="adresse" name="adresse" required>
+
+                <label for="codepostal">Code postal :</label>
+                <input type="text" id="codepostal" name="codepostal" required>
+
+                <label for="dateNaissance">Date de naissance :</label>
+                <input type="date" id="dateNaissance" name="dateNaissance" required>
+
+                <label for="villeNaissance">Ville de naissance :</label>
+                <input type="text" id="villeNaissance" name="villeNaissance" required>
+
+                <label for="numSecu">Numéro de sécurité sociale :</label>
+                <input type="text" id="numSecu" name="numSecu" required>
+
+                <div class="btn-container">
+                    <button type="submit">Valider</button>
+                    <button type="reset">Vider</button>
                 </div>
             </form>
         </div>
---
+++ code/pages/Usager/traitement.php
@@ -0,0 +1,36 @@
+<html>
+    <body>
+        <?php
+            include "../../services/serviceUsager.php";
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $nom = $_POST['nom'];
+                $prenom = $_POST['prenom'];
+                $civilite = $_POST['civilite'];
+                $adresse = $_POST['adresse'];
+                $codepostal = $_POST['codepostal'];
+                $date = $_POST['dateNaissance'];
+                $dateNaissance = new DateTime($date);
+                $villeNaissance = $_POST['villeNaissance'];
+                $numSecu = $_POST['numSecu'];
+                echo '<h2>Données soumises :</h2>';
+                echo '<p><strong>Nom :</strong> '.$nom.'</p>';
+                echo '<p><strong>Prénom :</strong> '.$prenom.'</p>';
+                echo '<p><strong>Civilité :</strong> '.$civilite.'</p>';
+                echo '<p><strong>Adresse :</strong> '.$adresse.'</p>';
+                echo '<p><strong>Code postal :</strong> '.$codepostal.'</p>';
+                echo '<p><strong>Date de naissance :</strong> '.$date.'</p>';
+                echo '<p><strong>Ville de naissance :</strong> '.$villeNaissance.'</p>';
+                echo '<p><strong>Numéro de sécurité sociale :</strong> '.$numSecu.'</p>';
+                $usager = new Usager(1,$nom, $prenom, $civilite, 1,$adresse, $codepostal, $dateNaissance, $villeNaissance, $numSecu);
+                //$repoUsager = new RepoUsager();
+                //$resultat = $repoUsager->addUsager($usager);
+                self::addUsager($usager);
+                if ($resultat) {
+                    echo "Usager ajouté";
+                } else {
+                    echo "Erreur dans la requête : " . $mysqlClient->error;
+                }
+            }
+        ?>
+    </body>
+</html>
