bb357467d0ccd4134e76b0621ca41d135599849f
Titouan <titouan.herbaut@etu.iut-tlse3.fr>
2023-12-29 21:34:13 +0100

    added navigation buttons on index

--- code/pages/index.php
+++ code/pages/index.php
@@ -8,5 +8,27 @@
         <div>
             <h1>Bienvenue sur votre outil de gestion de votre Cabinet Médical</h1>
         </div>
+        <div class="boite-index">
+            <div class="btn-index">
+                <a href="/phpProjetS3/code/pages/Usager/listeUsager.php">
+                    <p>Accès à la liste des usagers</p>
+                </a>
+            </div>
+            <div class="btn-index">
+                <a href="">
+                    <p>Accès à la liste des médecins</p>
+                </a>
+            </div>
+            <div class="btn-index">
+                <a href="">
+                    <p>Accès à la liste des rendez-vous</p>
+                </a>
+            </div>
+            <div class="btn-index">
+                <a href="">
+                    <p>Accès aux statistiques</p>
+                </a>
+            </div>
+        </div>
     </body>
 </html>
