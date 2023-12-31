6c6be76d99d2aaf98a6ad2044a5fdbba1d0867f8
Titouan Herbaut <titouan.herbaut@etu.iut-tlse3.fr>
2023-12-31 12:38:13 +0100

    TODO pas de medecin referent choisie dans addUsager

--- code/pages/Usager/traitementAddUsager.php
+++ code/pages/Usager/traitementAddUsager.php
@@ -6,13 +6,18 @@
                 $nom = ucfirst($_POST['nom']);
                 $prenom = ucfirst($_POST['prenom']);
                 $civilite = $_POST['civilite'];
+                if ($_POST['medecinRef'] == "null") { 
+                    $medecinRef = 4; 
+                } else { 
+                    $medecinRef = $_POST['medecinRef']; 
+                }
                 $adresse = $_POST['adresse'];
                 $codepostal = $_POST['codepostal'];
                 $date = $_POST['dateNaissance'];
                 $dateNaissance = new DateTime($date);
                 $villeNaissance = ucfirst($_POST['villeNaissance']);
                 $numSecu = $_POST['numSecu'];
-                $usager = new Usager(1,$nom, $prenom, $civilite, 4,$adresse, $codepostal, $dateNaissance, $villeNaissance, $numSecu);
+                $usager = new Usager(1,$nom, $prenom, $civilite, $medecinRef, $adresse, $codepostal, $dateNaissance, $villeNaissance, $numSecu);
                 $repoUsager = new RepoUsager();
                 $resultat = $repoUsager->addUsager($usager);
                 if ($resultat) {
