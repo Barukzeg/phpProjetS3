537b19f2e80a942ac61dea751eb080d2740110a1
Titouan <titouan.herbaut@etu.iut-tlse3.fr>
2024-01-02 22:00:19 +0100

    small update header
    les boutons sont cliquables partout

--- code/pages/header.php
+++ code/pages/header.php
@@ -9,30 +9,22 @@
         <!-- TODO modif le titre pour que ce soit jolie-->
         
         <div class="boite-boutons">
-            <div class="btn">
-                <a href="/phpProjetS3/code/pages/Usager/listeUsager.php">
-                    <img src="/phpProjetS3/code/image/iconUsager.png" alt="icon User">
-                    <p>Accès à la liste des usagers</p>
-                </a>
-            </div>
-            <div class="btn">
-                <a href="/phpProjetS3/code/pages/Medecin/listeMedecin.php">
-                    <img src="/phpProjetS3/code/image/iconMedecin.png" alt="icon Medecin">
-                    <p>Accès à la liste des médecins</p>
-                </a>
-            </div>
-            <div class="btn">
-                <a href="">
-                    <img src="/phpProjetS3/code/image/iconRDV.png" alt="icon RDV">
-                    <p>Accès à la liste des rendez-vous</p>
-                </a>
-            </div>
-            <div class="btn">
-                <a href="/phpProjetS3/code/pages/Stats/statistiques.php">
-                    <img src="/phpProjetS3/code/image/iconStat.png" alt="icon Statistique">
-                    <p>Accès aux statistiques</p>
-                </a>
-            </div>
+            <a href="/phpProjetS3/code/pages/Usager/listeUsager.php" class="btn">
+                <img src="/phpProjetS3/code/image/iconUsager.png" alt="icon User">
+                <p>Accès à la liste des usagers</p>
+            </a>
+            <a href="/phpProjetS3/code/pages/Medecin/listeMedecin.php" class="btn">
+                <img src="/phpProjetS3/code/image/iconMedecin.png" alt="icon Medecin">
+                <p>Accès à la liste des médecins</p>
+            </a>
+            <a href="" class="btn">
+                <img src="/phpProjetS3/code/image/iconRDV.png" alt="icon RDV">
+                <p>Accès à la liste des rendez-vous</p>
+            </a>
+            <a href="/phpProjetS3/code/pages/Stats/statistiques.php" class="btn">
+                <img src="/phpProjetS3/code/image/iconStat.png" alt="icon Statistique">
+                <p>Accès aux statistiques</p>
+            </a>
         </div>
     </nav>
 </header>
