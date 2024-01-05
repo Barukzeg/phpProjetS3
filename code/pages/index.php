27e421aeb9963d390a1685bc6b555d7c5c441232
Titouan <titouan.herbaut@etu.iut-tlse3.fr>
2024-01-05 16:31:48 +0100

    update buttons in index

--- code/pages/index.php
+++ code/pages/index.php
@@ -9,25 +9,25 @@
             <h1>Bienvenue sur votre outil de gestion de votre Cabinet Médical</h1>
         </div>
         <div class="boite-index">
-            <div class="btn-index">
+            <div class="btn-index" id="index-left">
                 <a href="/phpProjetS3/code/pages/Usager/listeUsager.php">
                     <img src="/phpProjetS3/code/image/iconUsager.png" alt="icon User">
                     <p>Liste des usagers</p>
                 </a>
             </div>
-            <div class="btn-index">
+            <div class="btn-index" id="index-right">
                 <a href="/phpProjetS3/code/pages/Medecin/listeMedecin.php">
                     <img src="/phpProjetS3/code/image/iconMedecin.png" alt="icon Medecin">
                     <p>Liste des médecins</p>
                 </a>
             </div>
-            <div class="btn-index">
+            <div class="btn-index" id="index-left">
                 <a href="/phpProjetS3/code/pages/RendezVous/listeRDV.php">
                     <img src="/phpProjetS3/code/image/iconRDV.png" alt="icon RDV">
                     <p>Liste des rendez-vous</p>
                 </a>
             </div>
-            <div class="btn-index">
+            <div class="btn-index" id="index-right">
                 <a href="/phpProjetS3/code/pages/Stats/statistiques.php">
                     <img src="/phpProjetS3/code/image/iconStat.png" alt="icon Statistique">
                     <p>Statistiques</p>
