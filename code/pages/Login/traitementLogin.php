fa6443c4fd3bf26650603e7445fe85bd0f328012
Nail Lamarti <nail.lamarti@etu.iut-tlse3.fr>
2024-01-05 23:11:53 +0100

    css Erreur login
    base pour le css de toutes les transitions

--- code/pages/Login/traitementLogin.php
+++ code/pages/Login/traitementLogin.php
@@ -7,14 +7,24 @@
         header('Location: ../index.php'); // Rediriger vers la page d'accueil si l'authentification est réussie
         exit();
     } else {
-        echo "Nom d'utilisateur ou mot de passe incorrect.";
-        
         echo '
-        <div class="bouton-add">
-            <form action="login.php">
-                <button>Retour à la page de connexion</button>
-            </form>
-        </div>';
+        <html>
+            <!DOCTYPE HTML>
+            <head>
+                <title>Erreur</title>
+                <link rel="stylesheet" href="/phpProjetS3/code/style/styleTrans.css">
+            </head>
+            <body>
+                <div class="container">
+                    <h2>Nom d\'utilisateur ou mot de passe incorrect.</h2>
+                    <div class="bouton">
+                        <form action="login.php">
+                            <button>Retour à la page de connexion</button>
+                        </form>
+                    </div>
+                </div>
+            </body>
+        </html>';
     }
 
 ?>
