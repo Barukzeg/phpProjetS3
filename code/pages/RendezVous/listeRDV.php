ee81c38f1b91efb0f9f3de8079662f02b06d0857
Titouan <titouan.herbaut@etu.iut-tlse3.fr>
2024-01-05 19:12:12 +0100

    update display of rdv and add listeRDV.css
    j'ai add aussi une fonction non chronologique

--- code/pages/RendezVous/listeRDV.php
+++ code/pages/RendezVous/listeRDV.php
@@ -4,7 +4,7 @@
     <head>
         <title>Liste des usagers</title>
         <link rel="stylesheet" href="/phpProjetS3/code/style/style.css">
-        <link rel="stylesheet" href="/phpProjetS3/code/style/listeUsager.css">
+        <link rel="stylesheet" href="/phpProjetS3/code/style/listeRDV.css">
     </head>
     <!-- TODO réparer le header
     parce que la ca marche mais c'est chemin absolu
@@ -25,49 +25,50 @@
                 include_once "../../modele/Usager.php";
 
                 try{
-                    $resultat = serviceRendezVous::getService()->getRDVChronological();
-                    echo '<div class="affichageResult">
-                    <table>
-                        <tr>
-                            <th>Date</th>
-                            <th>Heure</th>
-                            <th>Durée</th>
-                            <th>Usager</th>
-                            <th>Médecin</th>
-                            <th>Modifier</th>
-                            <th>Supprimer</th>
-                        </tr>';
-                    if (!empty($resultat)) {
-                        foreach ($resultat as $row) {
-                            echo '
+                    $resultat = serviceRendezVous::getService()->getRDVNonChronological();
+                    echo '
+                    <div class="affichageResult">
+                        <table>
                             <tr>
-                                <td>'.$row->getDateEtHeure()->format('Y-m-d').'</td>
-                                <td>'.$row->getDateEtHeure()->format('H:i').'</td>
-                                <td>'.$row->getDureeMinutes().' min</td>
-                                <td>'.serviceUsager::getService()->get($row->getIdClient())->getNom().' '.serviceUsager::getService()->get($row->getIdClient())->getPrenom().'</td>
-                                <td>'.serviceMedecin::getService()->get($row->getIdMedecin())->getNom().' '.serviceMedecin::getService()->get($row->getIdMedecin())->getPrenom().'</td>
-                                <td>
-                                    <form action="modRDV.php" method="post">
-                                        <input type="hidden" name="idC" value="'.$row->getIdClient().'">
-                                        <input type="hidden" name="idM" value="'.$row->getIdMedecin().'">
-                                        <input type="hidden" name="DateEtHeure" value="'.$row->getDateEtHeure()->format('Y-m-d H:i').'">
-                                        <button type="submit">Modifier</button>
-                                    </form>
-                                </td>
-                                <td>
-                                    <form action="traitementDeleteRDV.php" method="post">
-                                        <input type="hidden" name="idC" value="'.$row->getIdClient().'">
-                                        <input type="hidden" name="idM" value="'.$row->getIdMedecin().'">
-                                        <input type="hidden" name="DateEtHeure" value="'.$row->getDateEtHeure()->format('Y-m-d H:i').'">
-                                        <button type="submit">Supprimer</button>
-                                    </form>
-                                </td>
-                            ';
+                                <th>Date</th>
+                                <th>Heure</th>
+                                <th>Durée</th>
+                                <th>Usager</th>
+                                <th>Médecin</th>
+                                <th>Modifier</th>
+                                <th>Supprimer</th>
+                            </tr>';
+                        if (!empty($resultat)) {
+                            foreach ($resultat as $row) {
+                                echo '
+                                <tr>
+                                    <td>'.$row->getDateEtHeure()->format('d/m/Y').'</td>
+                                    <td>'.$row->getDateEtHeure()->format('H:i').'</td>
+                                    <td>'.$row->getDureeMinutes().' min</td>
+                                    <td>'.serviceUsager::getService()->get($row->getIdClient())->getNom().' '.serviceUsager::getService()->get($row->getIdClient())->getPrenom().'</td>
+                                    <td>'.serviceMedecin::getService()->get($row->getIdMedecin())->getNom().' '.serviceMedecin::getService()->get($row->getIdMedecin())->getPrenom().'</td>
+                                    <td>
+                                        <form action="modRDV.php" method="post">
+                                            <input type="hidden" name="idC" value="'.$row->getIdClient().'">
+                                            <input type="hidden" name="idM" value="'.$row->getIdMedecin().'">
+                                            <input type="hidden" name="DateEtHeure" value="'.$row->getDateEtHeure()->format('Y-m-d H:i').'">
+                                            <button type="submit">Modifier</button>
+                                        </form>
+                                    </td>
+                                    <td>
+                                        <form action="traitementDeleteRDV.php" method="post">
+                                            <input type="hidden" name="idC" value="'.$row->getIdClient().'">
+                                            <input type="hidden" name="idM" value="'.$row->getIdMedecin().'">
+                                            <input type="hidden" name="DateEtHeure" value="'.$row->getDateEtHeure()->format('Y-m-d H:i').'">
+                                            <button type="submit">Supprimer</button>
+                                        </form>
+                                    </td>
+                                ';
+                            }
+                        } else {
+                            echo '<tr><td colspan="7">Aucun rendez-vous</td></tr>';
                         }
-                    } else {
-                        echo '<tr><td colspan="7">Aucun rendez-vous</td></tr>';
-                    }
-                    echo "</div>";
+                    echo '</div>';
                 }catch (Exception $e){
                     echo $e->getMessage();
                 }
